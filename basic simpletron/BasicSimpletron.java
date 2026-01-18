06f41c761cf17a51b9f437680d11a8cff8aa43a5
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2026-01-18 15:21:47 -0500

    Changed everything into the static version for this first version c:

--- basic simpletron/BasicSimpletron.java
+++ basic simpletron/BasicSimpletron.java
@@ -3,34 +3,33 @@ import java.util.Scanner;
 public class BasicSimpletron {
 
     //ATTRIBUTES
-    private int[] memory = new int[100];
-    private double accumulator = 0.0;
-    private int instructionCounter = 0;
-    private int operationCode = 0;
-    private int operand = 0;
-    private int instructionRegister = 0;
-
+    private static int[] memory = new int[100];
+    private static double accumulator = 0.0;
+    private static int instructionCounter = 0;
+    private static int operationCode = 0;
+    private static int operand = 0;
+    private static int instructionRegister = 0;
     //OPERATION CODES
     //Input and Output
-    private final int READ = 10;
-    private final int WRITE = 11;
+    private static final int READ = 10;
+    private static final int WRITE = 11;
 
     //Load and Store
-    private final int LOAD = 20;
-    private final int STORE = 21;
+    private static final int LOAD = 20;
+    private static final int STORE = 21;
 
     //Arithmethic Operations
-    private final int ADD = 30;
-    private final int SUBTRACT = 31;
-    private final int DIVIDE = 32;
-    private final int MULTIPLY = 33;
-    private final int MODULO = 34;
+    private static final int ADD = 30;
+    private static final int SUBTRACT = 31;
+    private static final int DIVIDE = 32;
+    private static final int MULTIPLY = 33;
+    private static final int MODULO = 34;
 
     //Control Operations
-    private final int BRANCH = 40;
-    private final int BRANCHNEG = 41;
-    private final int BRANCHZERO = 42;
-    private final int HALT = 43;
+    private static final int BRANCH = 40;
+    private static final int BRANCHNEG = 41;
+    private static final int BRANCHZERO = 42;
+    private static final int HALT = 43;
 
     //METHODS
 
@@ -41,24 +40,24 @@ public class BasicSimpletron {
         System.out.println("*** I will display the location number and a question mark (?). ***");
         System.out.println("*** You then type the word for that location. ***");
         System.out.println("*** Type -99999 to stop entering your program. ***");
-    
-        BasicSimpletron simpletron = new BasicSimpletron();
 
-        simpletron.loadProgram();
+
+        loadProgram();
 
         System.out.println("*** Program loading completed ***");
         System.out.println("*** Program execution begins  ***");
 
-        for(int i : simpletron.memory){
+        for(int i : memory){
             System.out.printf("%+05d%n", i);
         }
     }
 
+    //Leaving this but I'm going to work this basic version with static methods
     public BasicSimpletron() {
         //Default Constructor
     }
 
-    public void loadProgram(){
+    public static void loadProgram(){
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("*** Enter your program instructions ***");
@@ -79,7 +78,4 @@ public class BasicSimpletron {
 
         scanner.close();
     }
-
-    //Getters and Setters
-
 }
